1fbb7e7fa98f0bb9418621ba3031d39bfc62a5b8
Niranjanmajgaonkar <niranjanmajgaonkar76@gmail.com>
2025-10-06 12:45:29 +0530

    added the conflict for the testing

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('mainContent')
 <style>
-    /* This cammand is used for the testing add-profile-page  */
+try to create the conflict
     body, html {
         height: 85%;
         margin: 0;
